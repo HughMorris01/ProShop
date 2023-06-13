5ac180d08297b14bbc2ceff1729e8e48610e9aeb
HughMorris01 <gfarrell2@Live.com>
2023-06-13 08:36:37 -0400

    Add Product and Rating Components

---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Airpods Wireless Bluetooth Headphones',
+    image: '/images/airpods.jpg',
+    description:
+      'Bluetooth technology lets you connect it with compatable devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 89.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'Airpods Wireless Bluetooth Headphones',
+    image: '/images/alexa.jpg',
+    description:
+      'Bluetooth technology lets you connect it with compatable devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 89.99,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 11,
+  },
+  {
+    _id: '3',
+    name: 'Airpods Wireless Bluetooth Headphones',
+    image: '/images/camera.jpg',
+    description:
+      'Bluetooth technology lets you connect it with compatable devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 89.99,
+    countInStock: 10,
+    rating: 3,
+    numReviews: 22,
+  },
+  {
+    _id: '4',
+    name: 'Airpods Wireless Bluetooth Headphones',
+    image: '/images/mouse.jpg',
+    description:
+      'Bluetooth technology lets you connect it with compatable devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 89.99,
+    countInStock: 10,
+    rating: 1,
+    numReviews: 7,
+  },
+  {
+    _id: '5',
+    name: 'Airpods Wireless Bluetooth Headphones',
+    image: '/images/phone.jpg',
+    description:
+      'Bluetooth technology lets you connect it with compatable devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 89.99,
+    countInStock: 10,
+    rating: 2.5,
+    numReviews: 8,
+  },
+  {
+    _id: '6',
+    name: 'Airpods Wireless Bluetooth Headphones',
+    image: '/images/playstation.jpg',
+    description:
+      'Bluetooth technology lets you connect it with compatable devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 89.99,
+    countInStock: 10,
+    rating: 3.5,
+    numReviews: 5,
+  },
+];
+
+export default products;
---
+++ frontend/src/screens/ProductScreen.jsx
@@ -0,0 +1,12 @@
+import { useParams, Link } from 'react-router-dom';
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import Rating from '../components/Rating';
+import products from '../products';
+
+const ProductScreen = () => {
+  const { id: productId } = useParams();
+  const product = products.find((p) => p._id === productId);
+  console.log(product);
+  return <div>ProductScreen</div>;
+};
+export default ProductScreen;
