20029bdb128f62c2314edf7af312769e3f7eb328
HughMorris01 <gfarrell2@Live.com>
2023-09-21 10:45:48 -0400

    Update OrderScreen

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -62,10 +62,41 @@ const OrderScreen = () => {
     }
   }, [order, paypal, paypalDispatch, loadingPayPal, errorPayPal]);
 
-  function onApprove() {}
-  function onApproveTest() {}
-  function createOrder() {}
-  function onError() {}
+  function onApprove(data, actions) {
+    return actions.order.capture().then(async function (details) {
+      try {
+        await payOrder({ orderId, details });
+        refetch();
+        toast.success('Payment successful');
+      } catch (error) {
+        toast.error(error?.data?.message || error.message);
+      }
+    });
+  }
+  async function onApproveTest() {
+    await payOrder({ orderId, details: { payer: {} } });
+    refetch();
+    toast.success('Payment successful');
+  }
+  function createOrder(data, actions) {
+    return actions.order
+      .create({
+        purchase_units: [
+          {
+            amount: {
+              value: order.totalPrice,
+            },
+          },
+        ],
+      })
+      .then((orderId) => {
+        return orderId;
+      });
+  }
+
+  function onError(err) {
+    toast.error(err.message);
+  }
 
   return isLoading ? (
     <Loader />
